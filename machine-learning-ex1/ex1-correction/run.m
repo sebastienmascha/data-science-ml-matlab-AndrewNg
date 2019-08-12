c1059f7c9f4067e921df63489d4625accd5e629f
seb <sebastien.mascha@icloud.com>
2019-08-12 19:06:42 +0200

    first commit

---
+++ machine-learning-ex1/ex1-correction/run.m
@@ -0,0 +1,11 @@
+data = load('ex1data2.txt');
+X = data(:, 1:2);
+y = data(:, 3);
+m = length(y);
+
+% Print out some data points
+fprintf('First 10 examples from the dataset: \n');
+fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
+
+fprintf('Normalizing Features ...\n');
+[X mu sigma] = featureNormalize(X);
